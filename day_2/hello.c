d7d234e9b6f9aefda3beb4ea9249725bdfa50dc6
bomatson <bobby@carbonfive.com>
2015-05-20 09:49:00 -0400

    day 1 and 2 hello world

---
+++ day_2/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main () {
+  printf("heelloo");
+  printf("another line, like puts");
+  printf("another line\n, using a newline char\n");
+  printf("another line\n, using a newline char again\n");
+}
