7c97af2ab5e824c3e59fb7d02a4257008f2da119
bomatson <bobby@carbonfive.com>
2015-06-04 09:46:42 -0400

    just some discussion around constants

---
+++ day_13/constants.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* leading 0 for int is octal, while 0X is hexadecimal */
+/* technically, string is as an array of chars */
+
+/* CONSTANTS */
+/* A constant expression is evaluated at compilation rather than run time (#define)*/
+/* A string constant, or string literal is is a seq of zero or more chars surrounded in double quotes */
+
+/* Enumeration constants is a list of constant int values: */
+/* enum boolean { NO, YES }; */
+
+/* constant declarations in array parameters indicate that the functions does not change the arr: */
+/* int strlen(const char[]); */
